4a92a9a25d81ed6fabdd7f45d94612dcf8804762
3652365 <desmond.beyond@gmail.com>
2022-08-14 21:15:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,7 @@ sed -i 's/192.168.1.1/192.168.100.182/g' package/base-files/files/bin/config_gen
 mkdir package/tt/
 # git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/openwrt-smartdns
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/tt/openwrt-passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/tt/openwrt-passwall
 git clone https://github.com/sirpdboy/luci-app-netdata.git package/tt/luci-app-netdata
 git clone https://github.com/netdata/netdata.git package/tt/netdata
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/tt/luci-app-adguardhome
